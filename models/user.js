1e24128bdf87365c193f35acf3bf61120248c8fb
Joseph Song <song.josephmin@gmail.com>
2023-06-30 17:57:25 -0700

    user model change

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, "Username is required"],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanurmeric letters and be unique"]
+        match: [/^(?=.{1,100}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanurmeric letters and be unique"]
     },
 
     image: {
